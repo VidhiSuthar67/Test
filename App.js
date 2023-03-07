b0f176a54495223f6cc2f68d50b565754bb47ce0
Vidhi Suthar <125564553+VidhiSuthar67@users.noreply.github.com>
2023-03-07 15:02:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Quiz from "../src/Components/Quiz";
+
+function App() {
+  return (
+    <>
+      <Quiz></Quiz>
+    </>
+  );
+}
+
+export default App;
---
+++ QuizData.js
@@ -0,0 +1,32 @@
+export const QuizData = [
+  {
+    question: "Which launguage run in web browser?",
+    options: ["Java", "C", "Python", "Javascript"],
+    answer: 4,
+  },
+  {
+    question: "What does CSS stands for?",
+    options: [
+      "Central Style Sheets",
+      "Casecading Style Sheets",
+      "Casecading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    answer: 2,
+  },
+  {
+    question: "What does HTML stands for?",
+    options: [
+      "Hypertext Markup Language ",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Termials Motorboat Lamborginis",
+    ],
+    answer: 1,
+  },
+  {
+    question: "What year was JS launched?",
+    options: ["1996", "1995", "1994", "none of above"],
+    answer: 2,
+  },
+];
